c11cc0fbac1c54f8f3ad191078a6b827e1c6c071
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-19 12:21:35 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,5 @@
 
 int main() {
     
-    int c;
-    printf("Input a temperature (in Centigrade):");
-    scanf("%d",  &c);
-    int f = (9*c+(32*5))/5;
-    printf("%d degrees Fahrenheit.", f);
-
     return 0;
 }
